d5ab7380af51de9a1119c428f007f555d296179b
Untone <anton.rewin@gmail.com>
2024-11-18 22:33:37 +0300

    codegen-pass

--- src/graphql/query/core/comments-myrates.ts
+++ src/graphql/query/core/comments-myrates.ts
@@ -1,7 +1,7 @@
 import { gql } from '@urql/core'
 
 export default gql`
-  query CommentsMyRates($comments: [Int]!, $shout: Int!) {
+  query CommentsMyRates($comments: [Int!]!, $shout: Int!) {
     get_my_rates_comments(comments: $comments, shout: $shout) {
       comment_id
       my_rate
