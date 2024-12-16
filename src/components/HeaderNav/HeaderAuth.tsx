0178f315c6aa9d5ef5c82ed4ef2a1e1e7ca00334
Untone <anton.rewin@gmail.com>
2024-12-16 13:15:45 +0300

    hydration-fix

--- src/components/HeaderNav/HeaderAuth.tsx
+++ src/components/HeaderNav/HeaderAuth.tsx
@@ -1,7 +1,6 @@
 import { A, useLocation, useNavigate } from '@solidjs/router'
 import { clsx } from 'clsx'
 import { Show, createMemo, createSignal, onCleanup, onMount } from 'solid-js'
-import { NoHydration } from 'solid-js/web'
 import { ShoutForm, useEditorContext } from '~/context/editor'
 import { useLocalize } from '~/context/localize'
 import { useNotifications } from '~/context/notifications'
@@ -32,7 +31,7 @@ const MD_WIDTH_BREAKPOINT = 992
 export const HeaderAuth = (props: Props) => {
   const { t } = useLocalize()
   const { showModal } = useUI()
-  const { session, isSessionLoaded } = useSession()
+  const { session } = useSession()
   const author = createMemo<Author>(() => session()?.user?.app_data?.profile as Author)
   const { unreadNotificationsCount, showNotificationsPanel } = useNotifications()
   const { form, toggleEditorPanel, publishShout } = useEditorContext()
@@ -52,9 +51,6 @@ export const HeaderAuth = (props: Props) => {
   const isNotificationsVisible = createMemo(() => session()?.access_token && !isEditorPage())
   const isSaveButtonVisible = createMemo(() => session()?.access_token && isEditorPage())
   const isCreatePostButtonVisible = createMemo(() => !isEditorPage())
-  const isAuthenticatedControlsVisible = createMemo(
-    () => session()?.access_token && session()?.user?.email_verified
-  )
 
   const handleBurgerButtonClick = () => {
     toggleEditorPanel()
@@ -119,195 +115,180 @@ export const HeaderAuth = (props: Props) => {
     }
   }
   return (
-    <NoHydration>
-      <Show when={isSessionLoaded()} keyed={true}>
-        <div class={clsx('col-auto col-lg-7', styles.usernav)}>
-          <div class={styles.userControl}>
-            <Show when={isCreatePostButtonVisible() && session()?.access_token}>
-              <div
-                class={clsx(
-                  styles.userControlItem,
-                  styles.userControlItemVerbose,
-                  styles.userControlItemCreate
-                )}
+    <div class={clsx('col-auto col-lg-7', styles.usernav)}>
+      <div class={styles.userControl}>
+        <Show when={isCreatePostButtonVisible() && session()?.access_token}>
+          <div
+            class={clsx(
+              styles.userControlItem,
+              styles.userControlItemVerbose,
+              styles.userControlItemCreate
+            )}
+          >
+            <A href="/edit/new">
+              <span class={styles.textLabel}>{t('Create post')}</span>
+              <Icon name="pencil-outline" class={styles.icon} />
+              <Icon name="pencil-outline-hover" class={clsx(styles.icon, styles.iconHover)} />
+            </A>
+          </div>
+        </Show>
+
+        <Show when={!isSaveButtonVisible()}>
+          <div class={clsx(styles.userControlItem, styles.userControlItemSearch)}>
+            <A href="?m=search">
+              <Icon name="search" class={styles.icon} />
+              <Icon name="search" class={clsx(styles.icon, styles.iconHover)} />
+            </A>
+          </div>
+        </Show>
+
+        <Show when={isNotificationsVisible()}>
+          <div class={styles.userControlItem} onClick={handleBellIconClick}>
+            <div class={styles.button}>
+              <Icon name="bell-white" counter={unreadNotificationsCount?.() || 0} class={styles.icon} />
+              <Icon
+                name="bell-white-hover"
+                counter={unreadNotificationsCount?.() || 0}
+                class={clsx(styles.icon, styles.iconHover)}
+              />
+            </div>
+          </div>
+        </Show>
+
+        <Show when={isSaveButtonVisible()}>
+          <Popup
+            trigger={
+              <span class={styles.editorModePopupOpener}>
+                <Icon name="swiper-r-arr" class={styles.editorModePopupOpenerIcon} />
+                {editorMode()}
+              </span>
+            }
+            popupCssClass={styles.editorPopup}
+          >
+            <ul class={clsx('nodash', styles.editorModesList)}>
+              <li
+                class={clsx({ [styles.editorModesSelected]: editorMode() === t('Preview') })}
+                onClick={() => setEditorMode(t('Preview'))}
               >
-                <A href="/edit/new">
-                  <span class={styles.textLabel}>{t('Create post')}</span>
-                  <Icon name="pencil-outline" class={styles.icon} />
-                  <Icon name="pencil-outline-hover" class={clsx(styles.icon, styles.iconHover)} />
-                </A>
-              </div>
-            </Show>
-
-            <Show when={!isSaveButtonVisible()}>
-              <div class={clsx(styles.userControlItem, styles.userControlItemSearch)}>
-                <A href="?m=search">
-                  <Icon name="search" class={styles.icon} />
-                  <Icon name="search" class={clsx(styles.icon, styles.iconHover)} />
-                </A>
-              </div>
-            </Show>
-
-            <Show when={isNotificationsVisible() || !author()}>
-              <div class={styles.userControlItem} onClick={handleBellIconClick}>
-                <div class={styles.button}>
-                  <Icon
-                    name="bell-white"
-                    counter={session() ? unreadNotificationsCount?.() || 0 : 1}
-                    class={styles.icon}
-                  />
-                  <Icon
-                    name="bell-white-hover"
-                    counter={session() ? unreadNotificationsCount?.() || 0 : 1}
-                    class={clsx(styles.icon, styles.iconHover)}
-                  />
+                <Icon name="eye" class={styles.editorModeIcon} />
+                <div class={styles.editorModeTitle}>{t('Preview')}</div>
+                <div class={styles.editorModeDescription}>
+                  Посмотрите, как материал будет выглядеть при публикации
                 </div>
-              </div>
-            </Show>
-
-            <Show when={isSaveButtonVisible()}>
-              <Popup
-                trigger={
-                  <span class={styles.editorModePopupOpener}>
-                    <Icon name="swiper-r-arr" class={styles.editorModePopupOpenerIcon} />
-                    {editorMode()}
-                  </span>
-                }
-                popupCssClass={styles.editorPopup}
+              </li>
+              <li
+                class={clsx({ [styles.editorModesSelected]: editorMode() === t('Editing') })}
+                onClick={() => setEditorMode(t('Editing'))}
               >
-                <ul class={clsx('nodash', styles.editorModesList)}>
-                  <li
-                    class={clsx({ [styles.editorModesSelected]: editorMode() === t('Preview') })}
-                    onClick={() => setEditorMode(t('Preview'))}
-                  >
-                    <Icon name="eye" class={styles.editorModeIcon} />
-                    <div class={styles.editorModeTitle}>{t('Preview')}</div>
-                    <div class={styles.editorModeDescription}>
-                      Посмотрите, как материал будет выглядеть при публикации
-                    </div>
-                  </li>
-                  <li
-                    class={clsx({ [styles.editorModesSelected]: editorMode() === t('Editing') })}
-                    onClick={() => setEditorMode(t('Editing'))}
-                  >
-                    <Icon name="pencil-outline" class={styles.editorModeIcon} />
-                    <div class={styles.editorModeTitle}>{t('Editing')}</div>
-                    <div class={styles.editorModeDescription}>Изменяйте текст напрямую в редакторе</div>
-                  </li>
-                  <li
-                    class={clsx({ [styles.editorModesSelected]: editorMode() === t('Commenting') })}
-                    onClick={() => setEditorMode(t('Commenting'))}
-                  >
-                    <Icon name="comment" class={styles.editorModeIcon} />
-                    <div class={styles.editorModeTitle}>{t('Commenting')}</div>
-                    <div class={styles.editorModeDescription}>
-                      Предлагайте правки и комментарии, чтобы сделать материал лучше
-                    </div>
-                  </li>
-                </ul>
-              </Popup>
-
-              <div class={clsx(styles.userControlItem, styles.userControlItemVerbose)}>
-                {renderIconedButton({
-                  value: t('Publish'),
-                  icon: 'publish',
-                  action: () => handlePublishClick(form)
-                })}
-              </div>
-
-              <div
-                class={clsx(
-                  styles.userControlItem,
-                  styles.settingsControlContainer,
-                  styles.userControlItemVerbose
-                )}
+                <Icon name="pencil-outline" class={styles.editorModeIcon} />
+                <div class={styles.editorModeTitle}>{t('Editing')}</div>
+                <div class={styles.editorModeDescription}>Изменяйте текст напрямую в редакторе</div>
+              </li>
+              <li
+                class={clsx({ [styles.editorModesSelected]: editorMode() === t('Commenting') })}
+                onClick={() => setEditorMode(t('Commenting'))}
               >
-                <Popover content={t('Settings')}>
-                  {(ref) => (
-                    <Button
-                      ref={ref}
-                      value={<Icon name="ellipsis" />}
-                      variant={'light'}
-                      onClick={handleBurgerButtonClick}
-                      class={styles.settingsControl}
-                    />
-                  )}
-                </Popover>
-              </div>
-            </Show>
-
-            <Show when={isCreatePostButtonVisible() && !session()?.access_token}>
-              <div
-                class={clsx(
-                  styles.userControlItem,
-                  styles.userControlItemVerbose,
-                  styles.userControlItemCreate
-                )}
-              >
-                <button onClick={handleCreatePostClick}>
-                  <span class={styles.textLabel}>{t('Create post')}</span>
-                  <Icon name="pencil-outline" class={styles.icon} />
-                  <Icon name="pencil-outline-hover" class={clsx(styles.icon, styles.iconHover)} />
-                </button>
-              </div>
-            </Show>
-
-            <Show
-              when={isAuthenticatedControlsVisible()}
-              fallback={
-                <Show when={!session()?.access_token}>
-                  <div class={clsx(styles.userControlItem, styles.userControlItemVerbose, 'loginbtn')}>
-                    <A href="?m=auth&mode=login">
-                      <span class={styles.textLabel}>{t('Enter')}</span>
-                      <Icon name="key" class={styles.icon} />
-                      <Icon name="key" class={clsx(styles.icon, styles.iconHover)} />
-                    </A>
-                  </div>
-                </Show>
-              }
-            >
-              <Show when={!isSaveButtonVisible() && props.showInboxButton}>
-                <div
-                  class={clsx(
-                    styles.userControlItem
-                    // styles.userControlItemInbox
-                  )}
-                >
-                  <A href={'/inbox'}>
-                    <div classList={{ entered: Boolean(matchInbox()) }}>
-                      <Icon name="inbox-white" class={styles.icon} />
-                      <Icon name="inbox-white-hover" class={clsx(styles.icon, styles.iconHover)} />
-                    </div>
-                  </A>
+                <Icon name="comment" class={styles.editorModeIcon} />
+                <div class={styles.editorModeTitle}>{t('Commenting')}</div>
+                <div class={styles.editorModeDescription}>
+                  Предлагайте правки и комментарии, чтобы сделать материал лучше
                 </div>
-              </Show>
-            </Show>
+              </li>
+            </ul>
+          </Popup>
+
+          <div class={clsx(styles.userControlItem, styles.userControlItemVerbose)}>
+            {renderIconedButton({
+              value: t('Publish'),
+              icon: 'publish',
+              action: () => handlePublishClick(form)
+            })}
           </div>
 
-          <Show when={session()?.access_token}>
-            <ProfilePopup
-              onVisibilityChange={(isVisible) => {
-                props.setIsProfilePopupVisible(isVisible)
-              }}
-              containerCssClass={styles.control}
-              trigger={
-                <div class={clsx(styles.userControlItem, styles.userControlItemUserpic)}>
-                  <button class={styles.button}>
-                    <div classList={{ entered: Boolean(matchProfile()) }}>
-                      <Userpic
-                        size={'L'}
-                        name={author()?.name || ''}
-                        userpic={author()?.pic || ''}
-                        class={styles.userpic}
-                      />
-                    </div>
-                  </button>
-                </div>
-              }
-            />
-          </Show>
-        </div>
-      </Show>
-    </NoHydration>
+          <div
+            class={clsx(
+              styles.userControlItem,
+              styles.settingsControlContainer,
+              styles.userControlItemVerbose
+            )}
+          >
+            <Popover content={t('Settings')}>
+              {(ref) => (
+                <Button
+                  ref={ref}
+                  value={<Icon name="ellipsis" />}
+                  variant={'light'}
+                  onClick={handleBurgerButtonClick}
+                  class={styles.settingsControl}
+                />
+              )}
+            </Popover>
+          </div>
+        </Show>
+
+        <Show when={isCreatePostButtonVisible() && !session()?.access_token}>
+          <div
+            class={clsx(
+              styles.userControlItem,
+              styles.userControlItemVerbose,
+              styles.userControlItemCreate
+            )}
+          >
+            <button onClick={handleCreatePostClick}>
+              <span class={styles.textLabel}>{t('Create post')}</span>
+              <Icon name="pencil-outline" class={styles.icon} />
+              <Icon name="pencil-outline-hover" class={clsx(styles.icon, styles.iconHover)} />
+            </button>
+          </div>
+        </Show>
+
+        <Show when={!session()?.access_token}>
+          <div class={clsx(styles.userControlItem, styles.userControlItemVerbose, 'loginbtn')}>
+            <A href="?m=auth&mode=login">
+              <span class={styles.textLabel}>{t('Enter')}</span>
+              <Icon name="key" class={styles.icon} />
+              <Icon name="key" class={clsx(styles.icon, styles.iconHover)} />
+            </A>
+          </div>
+        </Show>
+
+        <Show when={!isSaveButtonVisible() && props.showInboxButton}>
+          <div
+            class={clsx(
+              styles.userControlItem
+              // styles.userControlItemInbox
+            )}
+          >
+            <A href={'/inbox'}>
+              <div classList={{ entered: Boolean(matchInbox()) }}>
+                <Icon name="inbox-white" class={styles.icon} />
+                <Icon name="inbox-white-hover" class={clsx(styles.icon, styles.iconHover)} />
+              </div>
+            </A>
+          </div>
+        </Show>
+
+        <Show when={session()?.access_token}>
+          <ProfilePopup
+            onVisibilityChange={props.setIsProfilePopupVisible}
+            containerCssClass={styles.control}
+            trigger={
+              <div class={clsx(styles.userControlItem, styles.userControlItemUserpic)}>
+                <button class={styles.button}>
+                  <div classList={{ entered: Boolean(matchProfile()) }}>
+                    <Userpic
+                      size={'L'}
+                      name={author()?.name || ''}
+                      userpic={author()?.pic || ''}
+                      class={styles.userpic}
+                    />
+                  </div>
+                </button>
+              </div>
+            }
+          />
+        </Show>
+      </div>
+    </div>
   )
 }
