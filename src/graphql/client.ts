4104465969d4177a6741867b04373e045257732f
Untone <anton.rewin@gmail.com>
2024-12-24 15:26:43 +0300

    fmt

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -66,12 +66,14 @@ export const graphqlClientCreate = (url: string, token = '', origin = 'discours.
     fetchOptions: () => ({
       headers: {
         'content-type': 'application/json',
-        'accept': 'application/graphql-response+json, application/graphql+json, application/json',
-        'origin': origin || (typeof window !== 'undefined' ? window.location.origin : new URL(url).origin),
+        accept: 'application/graphql-response+json, application/graphql+json, application/json',
+        origin: origin || (typeof window !== 'undefined' ? window.location.origin : new URL(url).origin),
         'cache-control': 'no-cache',
-        ...(token ? { 
-          'authorization': token.startsWith('Bearer ') ? token : `Bearer ${token}`
-        } : {})
+        ...(token
+          ? {
+              authorization: token.startsWith('Bearer ') ? token : `Bearer ${token}`
+            }
+          : {})
       },
       credentials: 'include',
       mode: 'cors'
