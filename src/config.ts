d9a37aee8fdfd273201fd296f2e0e9b4e2e0df29
Untone <anton.rewin@gmail.com>
2025-05-29 17:38:01 +0300

    redeploy

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const cdnUrl = import.meta.env.PUBLIC_CDN_URL || 'https://images.discours.io'
-export const coreApiUrl = import.meta.env.PUBLIC_CORE_API || 'https://coretest.discours.io'
+export const coreApiUrl = import.meta.env.PUBLIC_CORE_API || 'https://coretest.discours.io/graphql'
 export const chatApiUrl = import.meta.env.PUBLIC_CHAT_API || 'https://inbox.discours.io'
-export const authApiUrl = import.meta.env.PUBLIC_AUTH_API || 'https://auth.discours.io/graphql'
+export const authApiUrl = import.meta.env.PUBLIC_AUTH_API || import.meta.env.PUBLIC_CORE_API
 export const sseUrl = import.meta.env.PUBLIC_REALTIME_EVENTS || 'https://connect.discours.io'
 export const gaIdentity = import.meta.env.PUBLIC_GA_IDENTITY || 'G-LQ4B87H8C2'
 
