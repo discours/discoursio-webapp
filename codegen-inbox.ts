4c29c993bee39014ef20595e13cae19f26ab4758
Untone <anton.rewin@gmail.com>
2025-08-01 04:21:51 +0300

    cginbox

---
+++ codegen-inbox.ts
@@ -0,0 +1,24 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  // Используем только inbox схему
+  schema: '../inbox/schema.graphql',
+  documents: [], // Пока нет документов для inbox
+  generates: {
+    './src/graphql/generated/inbox-introspection.json': {
+      plugins: ['introspection'],
+      config: {
+        minify: true
+      }
+    },
+    './src/graphql/generated/inbox-schema.graphql': {
+      plugins: ['schema-ast'],
+      config: {
+        includeDirectives: false
+      }
+    }
+  }
+}
+
+export default config 
